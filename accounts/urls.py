68fc2cc361b9813c150fa0aba067547dc5743d2d
shadiyaali <shadiyackn@gmail.com>
2025-05-23 10:47:25 +0530

    company

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from  .views import *
 
 urlpatterns = [
-    
-    # company Management
+
     
     path('companies/', CompanyListCreateAPIView.as_view(), name='company-list-create'),
     path('company/create/', CompanyCreateAPIView.as_view(), name='company-list-create'),
