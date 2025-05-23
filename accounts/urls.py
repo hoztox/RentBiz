6d0865dc46e7c999b694714eb9948346f25e9cda
shadiyaali <shadiyackn@gmail.com>
2025-05-23 10:34:16 +0530

    host issue

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,9 @@ from  .views import *
 urlpatterns = [
     
     # company Management
-    path('companies/create/', CompanyCreateAPIView.as_view(), name='company-list-create'),
+    
     path('companies/', CompanyListCreateAPIView.as_view(), name='company-list-create'),
-   
+    path('company/create/', CompanyCreateAPIView.as_view(), name='company-list-create'),
     path('companies/<int:pk>/', CompanyDetailAPIView.as_view(), name='company-detail'),
        
 ]
