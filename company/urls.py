e56491b12546b7f2fed6b4bab5d83c538a4215b4
Ami Salaam <amisalaam9@gmail.com>
2025-06-03 12:37:21 +0530

    feat-(Taxes)-Add/List/Updating Fully completed tax integratiomns

--- company/urls.py
+++ company/urls.py
@@ -81,7 +81,9 @@ urlpatterns = [
     path('tenancy/<int:tenancy_id>/renew/', TenancyRenewalView.as_view(), name='tenancy-renew'),
     
     
-   
+    #Taxes
+    path('taxes/<int:company_id>/<int:tax_id>/', TaxesAPIView.as_view(), name='taxes'),
+    path('taxes/<int:company_id>/', TaxesAPIView.as_view(), name='taxes'),
     
 ]
 
