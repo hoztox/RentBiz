795678d62ba60b3d784d26356885087931423378
arfankv <arfn74u@gmail.com>
2025-06-30 10:10:19 +0530

    WIP: local changes to company urls

--- company/urls.py
+++ company/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from  .views import *
+from rentbiz.utils.dashboard import *
 
 urlpatterns = [
     
@@ -30,7 +31,6 @@ urlpatterns = [
     path('units/company/<int:company_id>/', UnitsByCompanyView.as_view(), name='units-by-company'),
     path('units/<int:building_id>/vacant-units/', VacantUnitsByBuildingView.as_view(), name='vacant-units-by-building'),
     path('units/<int:building_id>/occupied-units/', OccupiedUnitsByBuildingView.as_view(), name='vacant-units-by-building'),
-    path('units/building/<int:building_id>/', BuildingUnitsByBuildingView.as_view(), name='vacant-units-by-building'),
    
     
     # unit type in masters
@@ -85,7 +85,6 @@ urlpatterns = [
     path('tenancy/<int:tenancy_id>/download-pdf/', TenancyHTMLPDFView.as_view(), name='tenancy-download-pdf'),
     path('tenancies/company/<company_id>/<unit_id>/',TenanciesByUnitView.as_view(), name='tenancies-by-unit'),
     path('tenancies/<int:tenancy_id>/terminate/', TerminateTenancyAPIView.as_view(), name='terminate-tenancy'),
-    path('tenancies/unit/<int:unit_id>/', TenancyByUnitView.as_view(), name='tenancies-by-uni'),
     
     
     #Taxes
@@ -114,6 +113,16 @@ urlpatterns = [
     # PaymentSchedule
     path('tenancies/<int:tenancy_id>/payment-schedules/', PaymentScheduleAPIView.as_view(), name='payment-schedule-list'),
     path('tenancies/<int:tenancy_id>/payment-schedules/<int:schedule_id>/', PaymentScheduleAPIView.as_view(), name='payment-schedule-update'),
+
+
+
+    #Dashboard
+    path('dashboard/properties-summary/<int:company_id>/', PropertiesSummaryView.as_view(), name='properties-summary'),
+    path('dashboard/rent-collection/<int:company_id>/', RentCollectionView.as_view(), name='rent-collection'),
+    path('dashboard/tenency-expiring/<int:company_id>/', TenancyExpiringView.as_view(), name='tenency-expiring'),
+    path('dashboard/revenue-report/<int:company_id>/', FinancialReportView.as_view(), name='revenue-report'),
+   
+
    
 
 ]
